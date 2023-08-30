b612a16018d0217df0a3fe1f57b4cca7045e4968
Kaiser, Marvin <marvin.kaiser@bechtle.com>
2023-08-30 13:28:38 +0200

    Working BookingDataDisplay

--- pages/components/BookingPage/RecommendDataDisplay/BookingDataDisplay.tsx
+++ pages/components/BookingPage/RecommendDataDisplay/BookingDataDisplay.tsx
@@ -1,13 +1,12 @@
-import {CSSProperties, ReactElement, useEffect} from "react";
+import {CSSProperties, ReactElement, useEffect, useState} from "react";
 import {BookingFilterObject, RecommendationTicket} from "@/utils/types";
 import {getTimeAsString} from "@/utils/TimeDateHandler";
 import styles from "./BookingDataDisplay.module.css"
 export default function ({mydata,barWidth,filterData} : {mydata:Record<number,Array<RecommendationTicket>>, barWidth?:number, filterData:BookingFilterObject}):ReactElement {
-    //const [calculatedTimes]
+    const [displayData, setDisplayData] = useState<Record<number, Array<RecommendationTicket>>>()
     // Styling
 
     const balkenWidth = barWidth ?? 800;
-    const keys = Object.keys(mydata);
     const minuteWidth = balkenWidth/(23*60 + 59) *2;
 
     // Styling END
@@ -26,6 +25,7 @@ export default function ({mydata,barWidth,filterData} : {mydata:Record<number,Ar
         for(let i = 0; i < availableTimes.length; i++){
             const isEarliestEntry = i==0;
             const isLast:boolean = (i+1==availableTimes.length);
+            // Zwischendrin
             if(!isLast && availableTimes[i].endDate < availableTimes[i+1].startDate){
                 // Push a blocker entry
                 console.log("EINTRAG " + i + " von " + availableTimes.length)
@@ -36,17 +36,37 @@ export default function ({mydata,barWidth,filterData} : {mydata:Record<number,Ar
                     plate: null,
                     name: availableTimes[i].name
                 })
-                console.log("HABE BLOCKER HINZUGEFÜGT:")
-                console.log(getTimeAsString(new Date(availableTimes[i].endDate)) + " bis " + getTimeAsString(new Date(availableTimes[i+1].startDate)))
+                continue;
+            }
+            if(isEarliestEntry){
+                if(availableTimes[i].startDate > lowestDate.getTime()){
+                    blockers.push({
+                        startDate: lowestDate.getTime(),
+                        name: availableTimes[i].name,
+                        plate: null,
+                        endDate: availableTimes[i].startDate,
+                        bookingId: 9999
+                    })
+                }
             }
         }
         return blockers;
     }
     useEffect(() => {
-        let block= calculateBlockedTimes(0);
-        console.log(block)
-        mydata[0].push(...block)
-        console.log(mydata)
+        if(!mydata){
+            return
+        }
+        let blockDaten: Record<number,Array<RecommendationTicket>> = {};
+        const keyset = Object.keys(mydata);
+        for(let i = 0; i < keyset.length; i++){
+            const sensorId = Number(keyset[i]);
+            let blocks = calculateBlockedTimes(sensorId);
+            blocks.push(...mydata[sensorId])
+            blockDaten[sensorId] =  blocks;
+        }
+
+        setDisplayData(blockDaten)
+
     },[mydata])
     return (
         <>
@@ -56,14 +76,14 @@ export default function ({mydata,barWidth,filterData} : {mydata:Record<number,Ar
                         <th>SensorId</th><th>Balken</th>
                     </tr>
 
-                {
-                    keys.map(key => (
+                { displayData &&
+                    Object.keys(displayData).map(key => (
                         <tr>
                             <td>{key}</td>
                             <td>
                                 <div className={styles.dataBar} style={{width: balkenWidth + "px"}}>
                                     {
-                                        mydata[Number(key)].map(ticket => (
+                                        displayData[Number(key)].map(ticket => (
                                             <section style={{
                                                 width: (ticket.endDate - ticket.startDate)/1000/60 / 2 * minuteWidth,
                                                 marginLeft: (ticket.startDate - lowestDate.getTime()) / 1000/ 60 / 2 * minuteWidth,
